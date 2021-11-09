9aa5906feef9a380316f8f716db38a32476d5fa3
Matthew Hillmer <71785471+Hillmdog@users.noreply.github.com>
2021-11-08 19:29:29 -0600

    Update js.js

--- HW4/js.js
+++ HW4/js.js
@@ -1,6 +1,6 @@
 function setup() {
     var canvas = document.getElementById('myCanvas');
-     var checkBox = document.getElementById("myCheck")
+    var checkBox = document.getElementById("myCheck")
     var ctx = canvas.getContext('2d');
     var t = 0
     var dy = 0;
@@ -17,22 +17,37 @@ function setup() {
                         y:Math.floor(Math.random() * (1000 - 630 + 1)) + 630, dy:0,
                         r:Math.floor(Math.random() * (15 - 4 + 1)) + 4});
     }
+    //kelp
+    var tParam = 0;
+
+    var p0=[0,-20,0];
+    var d0=[-100,50,0];
+    var p1=[0,100,0];
+    var d1=[-100,0,0];
+    var p2=[0,200,0];
+    var d2=[-100,0,0];
+    var p3=[0,300,0];
+    var d3=[-100,0,0];
+
+    var P0 = [p0,d0,p1,d1]; // First two points and tangents
+    var P1 = [p1,d1,p2,d2]; // Second two points and tangents
+    var P2 = [p2,d2,p3,d3]; // Second two points and tangents
     function draw() {
-            window.requestAnimationFrame(draw);
-            stack =[mat3.create()];
-            canvas.width=canvas.width;
-            if(dy != -2000){
-                dy = dy - 1;
-            }
-            else{
-                dy = 0;
-            }
-            if(dx != 1400){
-                dx = dx + .5;
-            }
-            else{
-                dx = -100
-            }
+        window.requestAnimationFrame(draw);
+        stack =[mat3.create()];
+        canvas.width=canvas.width;
+        if(dy != -2000){
+            dy = dy - 1;
+        }
+        else{
+            dy = 0;
+        }
+        if(dx != 1400){
+            dx = dx + .5;
+        }
+        else{
+            dx = -100
+        }
         function ocean(){
             var grd = ctx.createLinearGradient(0, 0, 0, 500);
             grd.addColorStop(0, " #0077b3");
@@ -87,7 +102,126 @@ function setup() {
                 }
             }
         }
+        //kelp and curves
+        if(tParam >= 2){
+            tParam = 0;
+        }
+        else{
+            tParam = tParam + .001;
+        }
+        function moveToTx(loc,Tx){
+            var res=vec3.create(); vec3.transformMat4(res,loc,Tx); ctx.moveTo(res[0],res[1]);
+        }
+
+        function lineToTx(loc,Tx){
+            var res=vec3.create(); vec3.transformMat4(res,loc,Tx); ctx.lineTo(res[0],res[1]);
+        }
+
+        function drawObject(color,Tx) {
+            ctx.beginPath();
+            ctx.fillStyle = color;
+            moveToTx([-5,-5,0],Tx);
+            lineToTx([-5,5,0],Tx);
+            lineToTx([5,5,0],Tx);
+            lineToTx([5,-5,0],Tx);
+            ctx.closePath();
+            ctx.fill();
+        }
+
+        var Hermite = function(t) {
+            return [
+            2*t*t*t-3*t*t+1,
+            t*t*t-2*t*t+t,
+            -2*t*t*t+3*t*t,
+            t*t*t-t*t
+            ];
+        }
+
+        var HermiteDerivative = function(t) {
+            return [
+            6*t*t-6*t,
+            3*t*t-4*t+1,
+            -6*t*t+6*t,
+            3*t*t-2*t
+            ];
+        }
+
+        function Cubic(basis,P,t){
+            var b = basis(t);
+            var result=vec3.create();
+            vec3.scale(result,P[0],b[0]);
+            vec3.scaleAndAdd(result,result,P[1],b[1]);
+            vec3.scaleAndAdd(result,result,P[2],b[2]);
+            vec3.scaleAndAdd(result,result,P[3],b[3]);
+            return result;
+        }
+        var C0 = function(t_) {return Cubic(Hermite,P0,t_);};
+        var C1 = function(t_) {return Cubic(Hermite,P1,t_);};
+        var C2 = function(t_) {return Cubic(Hermite,P2,t_);};
+        var C0prime = function(t_) {return Cubic(HermiteDerivative,P0,t_);};
+        var C1prime = function(t_) {return Cubic(HermiteDerivative,P1,t_);};
+        var Ccomp = function(t) {
+            if (t<1){
+                var u = t;
+                return C0(u);
+            }
+            else{
+                var u = t-1.0;
+                return C1(u);
+            }
+        }
+
+        var Ccomp_tangent = function(t) {
+            if (t<1){
+                var u = t;
+                return C0prime(u);
+            }
+            else{
+                var u = t-1.0;
+                return C1prime(u);
+            }
+        }
+
+        function drawTrajectory(t_begin,t_end,intervals,C,Tx,color,width) {
+            ctx.strokeStyle=color;
+            ctx.lineWidth=width;
+            ctx.beginPath();
+            moveToTx(C(t_begin),Tx);
+            for(var i=1;i<=intervals;i++){
+                var t=((intervals-i)/intervals)*t_begin+(i/intervals)*t_end;
+                lineToTx(C(t),Tx);
+            }
+            ctx.stroke();
+        }
         ocean();
+        //kelp
+        var kelpC = "#003300";
+        var kelpC2 = "#008000"
+        var Tblue_to_canvas = mat4.create();
+        mat4.fromTranslation(Tblue_to_canvas,[30,590,0]);
+        mat4.scale(Tblue_to_canvas,Tblue_to_canvas,[1,-1,-1]); // Flip the Y-axis
+        drawTrajectory(0.0,1.0,100,C0,Tblue_to_canvas,kelpC,20);
+        drawTrajectory(0.0,1.0,100,C1,Tblue_to_canvas,kelpC,20);
+        drawTrajectory(0.0,1.0,100,C0,Tblue_to_canvas,kelpC2,1);
+        drawTrajectory(0.0,1.0,100,C1,Tblue_to_canvas,kelpC2,1);
+        if (checkBox.checked == true){
+            var Tgreen_to_blue = mat4.create();
+            mat4.fromTranslation(Tgreen_to_blue,Ccomp(tParam));
+            var Tgreen_to_canvas = mat4.create();
+            var tangent = Ccomp_tangent(tParam);
+            var angle = Math.atan2(tangent[1],tangent[0]);
+            mat4.rotateZ(Tgreen_to_blue,Tgreen_to_blue,angle);
+            mat4.multiply(Tgreen_to_canvas, Tblue_to_canvas, Tgreen_to_blue);
+            drawObject("black",Tgreen_to_canvas);
+        }
+        mat4.translate(Tblue_to_canvas,Tblue_to_canvas,[50,0,0])
+        drawTrajectory(0.0,1.0,100,C0,Tblue_to_canvas,kelpC,20);
+        drawTrajectory(0.0,1.0,100,C1,Tblue_to_canvas,kelpC,20);
+        drawTrajectory(0.0,1.0,100,C2,Tblue_to_canvas,kelpC,20);
+        drawTrajectory(0.0,1.0,100,C0,Tblue_to_canvas,kelpC2,1);
+        drawTrajectory(0.0,1.0,100,C1,Tblue_to_canvas,kelpC2,1);
+        drawTrajectory(0.0,1.0,100,C2,Tblue_to_canvas,kelpC2,1);
+
         var tx = mat3.create();
         stack.unshift(mat3.clone(stack[0]));//context.save();
         bubble(0);
@@ -97,9 +231,6 @@ function setup() {
         shark();
         stack.shift();//context.restore();
         bubble(1);
-        if (checkBox.checked == false){
-
-        }
     }
     draw();
 }
